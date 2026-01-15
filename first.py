14c99eb6880fc9b74b62c951bb355efae9ebd918
iamithakur <asr62500@gmail.com>
2026-01-15 16:02:00 +0530

    linearSearch

---
+++ first.py
@@ -0,0 +1,8 @@
+num1=int(input("Enter the first no.: "))
+num2=int(input("Enter the second no.: "))
+
+sum = num1+num2
+print ("sum of no is= ", sum)
+avg = sum/2
+
+print("Average of the no. is= ", avg)
